5d8a633476376fc38e4a7a5e0abd5035e0996767
Andrew Dziabo <jameszabo@yahoo.com>
2022-09-12 20:40:38 -0400

    prelim api and json server interaction

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import MusicContainer from './MusicContainer';
 import NavBar from './NavBar';
 import Search from './Search';
@@ -6,12 +7,71 @@ import SavedMusicContainer from './SavedMusicContainer';
 
 
 function App() {
+  const CLIENT_ID = "b9cd3b08f037434b98664895837ca0e0"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
+
+  useEffect(() => {
+    const hash = window.location.hash     //set hash equal to information in url
+    let token = window.localStorage.getItem("token")
+
+    //If there is no token and there is a url, then get the token from the url
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+  }, [])
+
+  function searchArtists(e) {
+    e.preventDefault();
+    fetch(`https://api.spotify.com/v1/search?q=${searchKey}&type=artist&include_external=audio`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    })
+      .then(res => res.json())
+      .then(data => setArtists(data.artists.items))
+  }
+
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
   return (
     <div>
       <NavBar />
-      <Search />
-      <MusicContainer />
-      <SavedMusicContainer />
+      {!token ?
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+        : 
+        <button onClick={logout}>Logout</button>}
+
+      {token ?
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+          <button type={"submit"}>Search</button>
+        </form>
+
+        : <h2>Please login</h2>
+        }
+
+  
+        <Search />
+        {artists.map((artist) => <MusicContainer key={artist.id} artist={artist} token={token} />)}
+        <SavedMusicContainer />
+      
     </div>
   );
 }
